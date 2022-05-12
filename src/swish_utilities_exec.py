fe898dff2bcb1561d789da450ce8727201879a95
Ilya Kessel <ilya.kessel@deepcoding.ai>
2022-05-12 09:15:46 +0300

    for pip install

--- src/swish_utilities_exec.py
+++ src/swish_utilities_exec.py
@@ -6,10 +6,11 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir) 
 
-import run
+from run import run_with_args
 from click.testing import CliRunner
 
 def execute(args):
-    runner = CliRunner()
-    result = runner.invoke(run.cli, args, catch_exceptions=False)
+    # runner = CliRunner()
+    result = run_with_args(**{})
+    # result = runner.invoke(run.cli, args, catch_exceptions=False)
     return result
